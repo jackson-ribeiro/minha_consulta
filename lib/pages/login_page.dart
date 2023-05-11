b7b3e9016ff9c674df523f313cb6ae2927f93db4
jackson-ribeiro <jacksonquest1@gmail.com>
2023-05-11 16:48:21 -0300

    adicionando campos email e senha

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -11,19 +14,76 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          //mensagem de boas vindas
-          Text('Olá Novamente!'),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 25),
+              //mensagem de boas vindas
+              Text(
+                'Olá!',
+                style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Bem-vindo(a) de volta, você fez falta!',
+                style: GoogleFonts.poppins(
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(height: 20),
 
-          // email
+              // email
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'E-mail',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              //senha
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Senha',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
 
-          //senha
+              // botão de login
 
-          // botão de login
-
-          // botão de cadastro
-        ],
+              // botão de cadastro
+            ],
+          ),
+        ),
       ),
     );
   }
